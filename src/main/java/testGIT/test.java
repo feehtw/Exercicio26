1f7b3b7b8f3404346ed41bdfe742334e7ed086ba
Unisul ADM <Unisul ADM@UNS07DT99165.animaedu.intranet>
2025-03-31 10:08:57 -0300

    Inicio do projeto

---
+++ src/main/java/testGIT/test.java
@@ -0,0 +1,17 @@
+
+
+package testGIT;
+import java.util.Scanner;
+public class test {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int qntd = 20;
+        
+        for (int i = 0; i < qntd; i++) {
+            System.out.println("teste " + i);
+        }
+        
+        
+    }
+}
